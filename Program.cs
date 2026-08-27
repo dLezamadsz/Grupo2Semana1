cc54d19aec4147a377033dcabc52bf9a10a8d65b
Jesus <paredes.quispe23@gmail.com>
2026-08-27 18:42:40 -0500

    Ejercicio 1 Jesus

--- Program.cs
+++ Program.cs
@@ -10,7 +10,8 @@ namespace Grupo2Semana1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hola");
+           Ejercicio1 ejercicio1 = new Ejercicio1();
+            ejercicio1.Execute();
         }
     }
 }
