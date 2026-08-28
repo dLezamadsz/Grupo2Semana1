c9bac5723ad3f9ca3cd571f9fbd3b87dcb6c4054
Carlosz456 <karloszp001@gmail.com>
2026-08-27 20:06:54 -0500

    Menu principal se inicia primero

--- MenuPrincipal.cs
+++ MenuPrincipal.cs
@@ -6,31 +6,35 @@ using System.Threading.Tasks;
 
 namespace Grupo2Semana1
 {
-    internal class Switch
+    internal class MenuPrincipal
     {
         public void Seleccionar()
         {
-            Console.WriteLine("Ingrese un número del 1 al 5:");
+            Console.WriteLine("1 - Descubrir el area y perimetro de un rectangulo.");
+            Console.WriteLine("2 - Descubrir el area y periemtro de un triangulo");
+            Console.WriteLine("3 - Descubre que año fue bisiesto");
+            Console.WriteLine("4 - Calculadora simple");
+            Console.Write("\nIngrese un numero del 1 al 4: ");
             int numero = Convert.ToInt32(Console.ReadLine());
             switch (numero)
             {
                 case 1:
-                    Console.WriteLine("1 - Descubrir el area y perimetro de un rectangulo.");
+                    
                     Ejercicio1 ejercicio1 = new Ejercicio1();
                     ejercicio1.Execute();
                     break;
                 case 2:
-                    Console.WriteLine("2 - Descubrir el area y periemtro de un triangulo");
+                    
                     TrianguloArea trianguloArea = new TrianguloArea();  
                     trianguloArea.Calcular();
                     break;
                 case 3:
-                    Console.WriteLine("3 - Descubre que año fue bisiesto");
+                    
                     Bisiesto bisiesto = new Bisiesto();
                     bisiesto.Execute();
                     break;
                 case 4:
-                    Console.WriteLine("4 - Calculadora simple");
+                    
                     Ejercicio4 ejercicio4 = new Ejercicio4();
                     ejercicio4.Execute();
                     break;
--- Program.cs
+++ Program.cs
@@ -10,8 +10,8 @@ namespace Grupo2Semana1
     {
         static void Main(string[] args)
         {
-           Switch menu = new Switch();
-            menu.Seleccionar();
+            MenuPrincipal menuPrincipal = new MenuPrincipal();
+            menuPrincipal.Seleccionar();
         }
     }
 }
